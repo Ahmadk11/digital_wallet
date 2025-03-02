2fc9705d3d7c58085e82fd05b9599ab5f65061fd
Ahmadk11 <kaikatiahmad14@gmail.com>
2025-03-02 15:59:23 -0800

    Added help center controller

---
+++ server/user-v1/controllers/helpCenterController.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../../models/HelpCenter.php';
+require_once '../../connection/db.php';
+
+class HelpCenterController {
+    private $helpCenter;
+
+    public function __construct() {
+        global $conn;
+        $this->helpCenter = new HelpCenter($conn);
+    }
+
+    public function createArticle($title, $content, $category) {
+        $this->helpCenter->title = $title;
+        $this->helpCenter->content = $content;
+        $this->helpCenter->category = $category;
+        return $this->helpCenter->create();
+    }
+
+    public function getAllArticles() {
+        return $this->helpCenter->getAllArticles();
+    }
+
+    public function getArticlesByCategory($category) {
+        return $this->helpCenter->getArticlesByCategory($category);
+    }
+
+    public function searchArticles($keyword) {
+        return $this->helpCenter->searchArticles($keyword);
+    }
+}
+?>
