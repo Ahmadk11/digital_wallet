096038388cdb33725e772d1c796ab549c77c325b
Ahmadk11 <kaikatiahmad14@gmail.com>
2025-03-02 16:14:35 -0800

    Added help center routes

---
+++ server/user-v1/routes/helpCenterRoutes.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../controllers/HelpCenterController.php';
+
+$helpCenterController = new HelpCenterController();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_GET['action'] == 'create_article') {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $category = $_POST['category'];
+
+    if ($helpCenterController->createArticle($title, $content, $category)) {
+        echo json_encode(['message' => 'Article created successfully']);
+    } else {
+        echo json_encode(['message' => 'Failed to create article']);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_GET['action'] == 'get_all_articles') {
+    echo json_encode($helpCenterController->getAllArticles());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_GET['action'] == 'get_articles_by_category') {
+    $category = $_GET['category'];
+    echo json_encode($helpCenterController->getArticlesByCategory($category));
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_GET['action'] == 'search_articles') {
+    $keyword = $_GET['keyword'];
+    echo json_encode($helpCenterController->searchArticles($keyword));
+}
+?>
