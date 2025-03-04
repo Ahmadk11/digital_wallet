715678c79910d25dd8c941a8af8edf68907a7601
Ahmadk11 <kaikatiahmad14@gmail.com>
2025-03-04 14:00:47 -0800

    Updated database connection

--- server/connection/db.php
+++ server/connection/db.php
@@ -4,10 +4,10 @@ $dbname = 'digital_wallet';
 $username = 'root';
 $password = '';
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully!";
 ?>
