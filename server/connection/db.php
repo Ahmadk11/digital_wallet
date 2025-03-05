e8ba388c28d2feee789842599a081bb89e31ce96
Ahmadk11 <kaikatiahmad14@gmail.com>
2025-03-04 18:53:41 -0800

    hotfix

--- server/connection/db.php
+++ server/connection/db.php
@@ -4,11 +4,11 @@ $dbname = 'digital_wallet';
 $username = 'root';
 $password = '';
 
-
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully!";
 ?>
